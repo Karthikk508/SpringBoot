a2d4b58f76561efa32c740086209313893a514b1
Karthikk508 <karthik.kandhasamy508@gmail.com>
2024-12-05 13:16:48 +0530

    Init commit

---
+++ learn-spring-framework/src/main/java/com/karthik/learn_spring_framework/CourseController.java
@@ -0,0 +1,11 @@
+package com.karthik.learn_spring_framework;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	
+	
+	
+
+}
---
+++ learn-spring-framework/src/test/java/com/karthik/learn_spring_framework/LearnSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthik.learn_spring_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
