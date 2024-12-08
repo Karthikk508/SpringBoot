19ce05c688e3b4a7b89385544487f1be70eec318
Karthikk508 <karthik.kandhasamy508@gmail.com>
2024-12-08 20:51:20 +0530

    Embedded servers JAR

--- learn-spring-framework/src/main/java/com/karthik/learn_spring_framework/CourseController.java
+++ learn-spring-framework/src/main/java/com/karthik/learn_spring_framework/CourseController.java
@@ -8,25 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
-	
-	
+
 	@RequestMapping("/courses")
-	public List<Course> retrieveAllCourses(){
-		
-		return Arrays.asList(new Course(1,"Learn spring","in28Minutes"),
-				             new Course(2,"Learn aws","in28Minutes"),
-				             new Course(3,"Learn springBoot","in28Minutes"),
-				             new Course(4,"Learn spring devtools","karthi"));
+	public List<Course> retrieveAllCourses() {
+
+		return Arrays.asList(new Course(1, "Learn spring", "in28Minutes"), new Course(2, "Learn aws", "in28Minutes"),
+				new Course(3, "Learn springBoot", "in28Minutes"), new Course(4, "Learn spring devtools", "karthi"));
 	}
-	
-	
-	
-	
-	
-	
 
 }
 
-
 //http://localhost:8080/courses
-
--- learn-spring-framework/src/main/java/com/karthik/learn_spring_framework/CurrencyControllerConfiguration.java
+++ learn-spring-framework/src/main/java/com/karthik/learn_spring_framework/CurrencyControllerConfiguration.java
@@ -1,32 +1,21 @@
 package com.karthik.learn_spring_framework;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyControllerConfiguration {
-	
-	
+
 	@Autowired
 	public CurrencyServiceConfiguration configuration;
-	
-	
-	@RequestMapping("/courses")
-	public CurrencyServiceConfiguration retrieveAllCourses(){
-		
+
+	@RequestMapping("/currency-configuration")
+	public CurrencyServiceConfiguration retrieveAllCourses() {
+
 		return configuration;
 	}
-	
-	
-	
-	
-	
-	
 
 }
 
-
 //http://localhost:8080/courses
-
